6320d805bb1d6b9c79e15fa18b51d6e486eeab73
SatoSouta <419030016@qq.com>
2020-04-03 13:12:13 +0800

    修正地址错误

--- Plugin.php
+++ Plugin.php
@@ -141,7 +141,7 @@ class APlayerAtBottom_Plugin implements Typecho_Plugin_Interface
       	if($aplayer === '0') {
         	echo '<link rel="stylesheet" href="'.Helper::options()->pluginUrl.'/APlayerAtBottom/static/APlayer.min.css">';
         }elseif($aplayer === '1'){
-			echo '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/SatoSouta/APlayerAtBottom/staitc/APlayer.min.css">';
+			echo '<link rel="stylesheet" href="//cdn.jsdelivr.net/gh/SatoSouta/APlayerAtBottom/static/APlayer.min.css">';
 		}elseif($aplayer === '2'){}
       	
       	//判断是否打开歌词
@@ -249,7 +249,7 @@ class APlayerAtBottom_Plugin implements Typecho_Plugin_Interface
       	if($aplayer === '0'){
 			echo '<script src="'.Helper::options()->pluginUrl.'/APlayerAtBottom/static/APlayer.min.js"></script>';
 		}elseif($aplayer === '1') {
-        	echo '<script src="//cdn.jsdelivr.net/gh/SatoSouta/APlayerAtBottom/staitc/APlayer.min.js"></script>';
+        	echo '<script src="//cdn.jsdelivr.net/gh/SatoSouta/APlayerAtBottom/static/APlayer.min.js"></script>';
         }elseif($aplayer === '2'){}
         
 		//输出配置js
